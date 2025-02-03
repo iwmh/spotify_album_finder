fa49545a237835bc9f371a393261ad112c6f8039
iwmh <hiroshiplusone@gmail.com>
2025-02-04 01:14:39 +0900

    move the project out of the root into the 'apps' folder.

--- android/app/src/main/kotlin/com/iwmh/spotify_album_finder/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.iwmh.spotify_album_finder
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ apps/flutter_album_finder/android/app/src/main/kotlin/com/example/flutter_album_finder/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_album_finder
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
