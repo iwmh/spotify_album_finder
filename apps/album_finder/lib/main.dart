28715cdebb170ab23a36f13a20934c0f2928e6d8
iwmh <hiroshiplusone@gmail.com>
2025-03-20 22:18:51 +0900

    routerを別ファイルに

--- apps/album_finder/lib/main.dart
+++ apps/album_finder/lib/main.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 
+import 'package:album_finder/router.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 
 void main() async {
   await runZonedGuarded<Future<void>>(
@@ -24,63 +24,7 @@ class AlbumFinder extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      routerConfig: _router,
-    );
-  }
-}
-
-/// The route configuration.
-final GoRouter _router = GoRouter(
-  routes: <RouteBase>[
-    GoRoute(
-      path: '/',
-      builder: (BuildContext context, GoRouterState state) {
-        return const HomeScreen();
-      },
-      routes: <RouteBase>[
-        GoRoute(
-          path: 'details',
-          builder: (BuildContext context, GoRouterState state) {
-            return const DetailsScreen();
-          },
-        ),
-      ],
-    ),
-  ],
-);
-
-/// The home screen
-class HomeScreen extends StatelessWidget {
-  /// Constructs a [HomeScreen]
-  const HomeScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Home Screen')),
-      body: const Center(
-        child: Text(
-          'you need to log in!!',
-        ),
-      ),
-    );
-  }
-}
-
-/// The details screen
-class DetailsScreen extends StatelessWidget {
-  /// Constructs a [DetailsScreen]
-  const DetailsScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Details Screen')),
-      body: const Center(
-        child: Text(
-          "Congratulations!!!\nYou're successfully logged in.",
-        ),
-      ),
+      routerConfig: buildRouter(),
     );
   }
 }
---
+++ apps/album_finder/lib/router.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+/// routing table を作成
+GoRouter buildRouter() {
+  return GoRouter(
+    routes: <RouteBase>[
+      GoRoute(
+        path: '/',
+        builder: (BuildContext context, GoRouterState state) {
+          return const HomeScreen();
+        },
+        routes: <RouteBase>[
+          GoRoute(
+            path: 'details',
+            builder: (BuildContext context, GoRouterState state) {
+              return const DetailsScreen();
+            },
+          ),
+        ],
+      ),
+    ],
+  );
+}
+
+/// The home screen
+class HomeScreen extends StatelessWidget {
+  /// Constructs a [HomeScreen]
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home Screen')),
+      body: const Center(
+        child: Text(
+          'you need to log in!!',
+        ),
+      ),
+    );
+  }
+}
+
+/// The details screen
+class DetailsScreen extends StatelessWidget {
+  /// Constructs a [DetailsScreen]
+  const DetailsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Details Screen')),
+      body: const Center(
+        child: Text(
+          "Congratulations!!!\nYou're successfully logged in.",
+        ),
+      ),
+    );
+  }
+}
